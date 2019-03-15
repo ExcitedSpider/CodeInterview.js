6578e259f0c1b4676521d74dc59b117924eef3eb
ExcitedQe <213161557@seu.edu.cn>
2019-03-15 09:29:13 +0800

    3

---
+++ 3_distinct.js
@@ -0,0 +1,23 @@
+/**
+ * 剑指Offer3 数组去重
+ * 不用书上那种限定条件的方法，而是一种比较通用的方法
+ */
+
+var arr = Array.from(new Array(1000000),()=>{
+  return Math.round(800000*Math.random()); 
+})
+
+var valid = {};
+var repeat = 0;
+var dateBegin = new Date();
+var result = [];
+
+arr.forEach(element => {
+  if(!valid[element])
+     result.push(arr);
+     valid[element]=1;
+});
+
+var dateEnd = new Date();
+console.log("time cost: "+(dateEnd.getTime()-dateBegin.getTime())+"ms");
+console.log("unique elements: "+result.length);
