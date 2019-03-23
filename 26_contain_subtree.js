cc1c676984ed24dae19dda2750f5f0cd2e408f27
ExcitedQe <213161557@seu.edu.cn>
2019-03-23 19:35:25 +0800

    2019/3/23

---
+++ 26_contain_subtree.js
@@ -0,0 +1,55 @@
+/**
+ * 剑指Offer 面试题26
+ * 判断树中是否包含子树
+ */
+
+function Node(value, leftChild, rightChild) {
+  this.value = value;
+  this.leftChild = leftChild;
+  this.rightChild = rightChild;
+}
+
+Node.prototype.hasSubtree = function (subroot) {
+  if(!subroot) return true;
+
+  let thisMatch = false, leftMatch = false, rightMatch = false;
+
+  if (this.value === subroot.value) {
+    thisMatch =  Node._match(this, subroot);
+  }
+  if (this.leftChild) {
+    leftMatch =  this.leftChild.hasSubtree(subroot);
+  }
+  if (this.rightChild){
+    rightMatch =  this.rightChild.hasSubtree(subroot);
+  }
+
+  return thisMatch || leftMatch || rightMatch;
+
+}
+
+Node._match = function(tree, subtree) {
+  if (!subtree) return true;
+  else if (!tree && subtree) return false;
+  else if (tree.value === subtree.value) 
+    return Node._match(tree.leftChild, subtree.leftChild) && Node._match(tree.rightChild, subtree.rightChild);
+}
+
+{
+  let root = new Node(0,new Node(1), new Node(2));
+  root.leftChild.leftChild = new Node(3);
+  root.leftChild.rightChild = new Node(4);
+  root.rightChild.leftChild = new Node(5);
+  root.rightChild.rightChild = new Node(6);
+
+  let assert = require('assert');
+  assert.equal(root.hasSubtree(new Node(2,undefined,new Node(6))),true);
+  assert.equal(root.hasSubtree(new Node(2,new Node(5),new Node(6))),true);
+  assert.equal(root.hasSubtree(new Node(2)),true);
+  assert.equal(root.hasSubtree(new Node(2,new Node(5))),true);
+  assert.equal(root.hasSubtree(new Node(2,new Node(6))),false);
+  assert.equal(root.hasSubtree(new Node(2,undefined,new Node(5))),false);
+  assert.equal(root.hasSubtree(),true);
+  
+  
+}
